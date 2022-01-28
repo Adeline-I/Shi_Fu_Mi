9a3a9d00d3f1c3a0147593275dea6a2b11c1f8a0
Adeline Izri <adeline.rudnicki@gmail.com>
2022-01-28 16:45:00 +0100

    Ajout

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,13 @@ function compareChoice(userRandom, computerRandom) {
     if (computerRandom == userRandom) {
         result = `égalité !`;
 
-    } else if (userRandom == 'ciseaux' && computerRandom == 'feuille') {
+    } else if (userRandom == 'Ciseaux' && computerRandom == 'Feuille') {
         result = `gagné(e) !`;
 
-    } else if (userRandom == 'pierre' && computerRandom == 'ciseaux') {
+    } else if (userRandom == 'Pierre' && computerRandom == 'Ciseaux') {
         result = `gagné(e) !`;
 
-    } else if (userRandom == 'feuille' && computerRandom == 'pierre') {
+    } else if (userRandom == 'Feuille' && computerRandom == 'Pierre') {
         result = `gagné(e) !`;
 
     } else {
@@ -23,7 +23,7 @@ function compareChoice(userRandom, computerRandom) {
 
     countRound ++;
     return result;
-}
+};
 
 
 //Choix de l'ordinateur.
@@ -32,57 +32,57 @@ function computerChoice() {
     let computerRandom = Math.round(Math.random() * 2);
         
     if  (computerRandom == 0) {
-        computerRandom = 'pierre';
+        computerRandom = 'Pierre';
     
     } else if  (computerRandom == 1) {
-        computerRandom = 'feuille';
+        computerRandom = 'Feuille';
     
     } else {
-        computerRandom = 'ciseaux';
+        computerRandom = 'Ciseaux';
 
     }
     return computerRandom;
-}
+};
 
 
 //Messages de Score.
 
 function scoreMessage(result) {
     if (result == `égalité !`) {
-        resultGame.innerHTML = `égalité !`;       
+        resultGame.innerHTML = `Égalité !`;       
 
     } else if (result == `gagné(e) !`) {
-        resultGame.innerHTML = `Vous avez gagné(e) !`;
+        resultGame.innerHTML = `Gagné(e) !`;
         userScoreBox++;
 
     } else {
-        resultGame.innerHTML = `Vous avez perdu !`;
+        resultGame.innerHTML = `Perdu !`;
         computerScoreBox++;
     }
 
     roundProgress.innerHTML = `Manche : ${countRound}`;
-    userScore.innerHTML = `Votre score est de ${userScoreBox}`;
-    computerScore.innerHTML = `Le score de l'ordinateur est de ${computerScoreBox}`;
-}
+    userScore.innerHTML = `Score : ${userScoreBox}`;
+    computerScore.innerHTML = `Score : ${computerScoreBox}`;
+};
 
 //Fin du jeu.
 
 function endGame(countRound, userScoreBox, computerScoreBox) {
     if (countRound == 3) {
-        console.log(`Fin de la Roundie.`)
         userRandomRock.setAttribute("disabled", "disabled");
         userRandomPaper.setAttribute("disabled", "disabled");
         userRandomScissors.setAttribute("disabled", "disabled");
         reloadGame.removeAttribute("disabled");
+        
         if (userScoreBox > computerScoreBox) {
             userWinPart ++;
-            console.log(userWinPart);
+
         } else if (userScoreBox < computerScoreBox) {
             computerWinPart ++;
         }
     }
-    userPartProgress.innerHTML = `Vous avez gagné ${userWinPart} partie(s).`;
-    computerPartProgress.innerHTML = `L'ordinateur a gagné ${computerWinPart} partie(s).`;
+    userPartProgress.innerHTML = `Partie(s) gagnée(s) : ${userWinPart}`;
+    computerPartProgress.innerHTML = `Partie(s) gagnée(s) : ${computerWinPart}`;
 };
 
 
@@ -93,12 +93,16 @@ let userRandomPaper = document.getElementById('userRandomPaper');
 let userRandomScissors = document.getElementById('userRandomScissors');
 let reloadGame = document.getElementById('reloadGame');
 
-let roundProgress = document.getElementById('roundProgress');
 let userScore = document.getElementById('userScore');
-let computerScore = document.getElementById('computerScore');
-let resultGame = document.getElementById('resultGame');
 let userPartProgress = document.getElementById('userPartProgress');
+let userRandomChoice = document.getElementById('userRandomChoice');
+
+let computerScore = document.getElementById('computerScore');
 let computerPartProgress = document.getElementById('computerPartProgress');
+let computerRandomChoice = document.getElementById('computerRandomChoice');
+
+let roundProgress = document.getElementById('roundProgress');
+let resultGame = document.getElementById('resultGame');
 
 
 
@@ -119,69 +123,61 @@ reloadGame.setAttribute("disabled", "disabled");
 //Choix de l'utilisateur : Pierre
 
 userRandomRock.addEventListener('click', () => {
-    let userRandom = 'pierre';
+    let userRandom = 'Pierre';
+    userRandomChoice.innerHTML = userRandom;
 
     //Choix de l'ordinateur.
-
     let computerRandom = computerChoice();
+    computerRandomChoice.innerHTML = computerRandom; 
 
     // Comparaison des 2 choix.
-
     let result = compareChoice(userRandom, computerRandom);
 
     //Messages de Score.
-
     scoreMessage(result);
 
     //Fin du jeu.
-
     endGame(countRound, userScoreBox, computerScoreBox);
 });
 
 //Choix de l'utilisateur : Feuille
 
 userRandomPaper.addEventListener('click', () => {
-    let userRandom = 'feuille';
+    let userRandom = 'Feuille';
+    userRandomChoice.innerHTML = userRandom;
 
     //Choix de l'ordinateur.
-
     let computerRandom = computerChoice();
+    computerRandomChoice.innerHTML = computerRandom;
 
     // Comparaison des 2 choix.
-
     let result = compareChoice(userRandom, computerRandom);
 
     //Messages de Score.
-
     scoreMessage(result);
 
     //Fin du jeu.
-
     endGame(countRound, userScoreBox, computerScoreBox);
-
 });
 
 //Choix de l'utilisateur : Ciseaux
 
 userRandomScissors.addEventListener('click', () => {
-    let userRandom = 'ciseaux';
+    let userRandom = 'Ciseaux';
+    userRandomChoice.innerHTML = userRandom;
 
     //Choix de l'ordinateur.
-
     let computerRandom = computerChoice();
+    computerRandomChoice.innerHTML = computerRandom;
 
     // Comparaison des 2 choix.
-
     let result = compareChoice(userRandom, computerRandom);
 
     //Messages de Score.
-
     scoreMessage(result);
 
     //Fin du jeu.
-
     endGame(countRound, userScoreBox, computerScoreBox);
-
 });
 
 //Choix de l'utilisateur : relancer le jeu
@@ -195,105 +191,3 @@ reloadGame.addEventListener('click', () => {
     userRandomScissors.removeAttribute("disabled");
     reloadGame.setAttribute("disabled", "disabled");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Correction
-
-// 2 joueurs (user, computer)
-// let user;
-// let computer;
-
-// let userChoice;
-
-// btnPierre.addEventListener('click', () => {
-//     userChoice = 'pierre';
-// });
-
-// user choisi parmi 3 choix (pierre, feuille, ciseaux)
-// computer choisi parmi 3 choix (pierre, feuille, ciseaux)
-
-// Comparer userChoice et computerChoice
-
-// si userChoice == computerChoice  
-//     résultat = 'égalité'
-
-// sinon si userChoice == feuille et computerChoice == ciseaux
-//     ou userChoice == ciseaux et computerChoice == pierre
-//     ou userChoice == pierre et computerChoice == feuille
-// Alors    
-//     résultat = 'défaite'
-
-// sinon    
-//     résultat = 'victoire'
-
-
-
-
-//     Selon résultat => victoire, égalité, défaite
-
-
-
-
-// Comparaison des 2 choix.
-
-// function compareChoice(userRandom, computerRandom) {
-
-//     if (computerRandom == 'ciseaux' && userRandom == 'feuille') {
-//         resultGame.innerHTML = `Les ciseaux coupent la feuille : vous avez perdu !`;
-//         computerScore++
-//         userScore.innerHTML = userScore;
-//         computerScore.innerHTML = computerScore;
-
-//     } else if (userRandom == 'ciseaux' && computerRandom == 'feuille') {
-//         resultGame.innerHTML = `Les ciseaux coupent la feuille : vous avez gagné(e) !`;
-//         userScore++
-//         userScore.innerHTML = userScore;
-//         computerScore.innerHTML = computerScore;
-
-//     } else if (computerRandom == 'pierre' && userRandom == 'ciseaux') {
-//         resultGame.innerHTML = `La pierre casse les ciseaux : vous avez perdu !`;
-//         computerScore++
-//         userScore.innerHTML = userScore;
-//         computerScore.innerHTML = computerScore;
-
-//     } else if (userRandom == 'pierre' && computerRandom == 'ciseaux') {
-//         resultGame.innerHTML = `La pierre casse les ciseaux : vous avez gagné(e) !`;
-//         resultGame.innerHTML = `Les ciseaux coupent la feuille : vous avez gagné(e) !`;
-//         userScore++
-//         userScore.innerHTML = userScore;
-//         computerScore.innerHTML = computerScore;
-
-//     } else if (computerRandom == 'feuille' && userRandom == 'pierre') {
-//         resultGame.innerHTML = `La feuille enveloppe la pierre : vous avez perdu !`;
-//         computerScore++
-//         userScore.innerHTML = userScore;
-//         computerScore.innerHTML = computerScore;
-
-//     } else if (userRandom == 'feuille' && computerRandom == 'pierre') {
-//         resultGame.innerHTML = `La feuille enveloppe la pierre : vous avez gagné(e) !`;
-//         resultGame.innerHTML = `Les ciseaux coupent la feuille : vous avez gagné(e) !`;
-//         userScore++
-//         userScore.innerHTML = userScore;
-//         computerScore.innerHTML = computerScore;
-
-//     } else {
-//         resultGame.innerHTML = `Égalité parfaite !`;
-//         resultGame.innerHTML = `Les ciseaux coupent la feuille : vous avez gagné(e) !`;
-//         userScore.innerHTML = userScore;
-//         computerScore.innerHTML = computerScore;
-//     }
